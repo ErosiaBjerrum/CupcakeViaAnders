5b91e1b983bfb5069306f7772815ea0efaabee75
SilasKongshoej <cph-sk559@stud.ek.dk>
2026-04-18 20:24:28 +0200

    First session of working on entities and mappers

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -1,4 +1,32 @@
 package app.entities;
 
 public class User {
+
+    private int id;
+    private String username;
+    private String password;
+    private int saldo;
+
+    public User(int id, String username, String password, int saldo) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.saldo = saldo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
 }
--- src/main/java/app/persistence/UserMapper.java
+++ src/main/java/app/persistence/UserMapper.java
@@ -1,4 +1,12 @@
 package app.persistence;
 
+import org.postgresql.jdbc2.optional.ConnectionPool;
+
 public class UserMapper {
+
+    // Ting til user: ID, Brugernavn, Password og Saldo
+    private final ConnectionPool connectionPool;
+
+
+
 }
